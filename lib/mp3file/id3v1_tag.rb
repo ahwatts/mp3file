32d7c9339dd2f89dd75050f00f70d178291bba82
Andrew Watts <ahwatts@gmail.com>
2010-12-19 15:36:06 -0500

    Obviously they're calld ID3v1 Tags, and not headers.  Duh.

--- lib/mp3file/id3v1_tag.rb
+++ lib/mp3file/id3v1_tag.rb
@@ -1,10 +1,10 @@
 module Mp3file
-  class InvalidID3v1HeaderError < Mp3fileError; end
+  class InvalidID3v1TagError < Mp3fileError; end
 
-  class ID3v1Header
+  class ID3v1Tag
     attr_accessor(:title, :artist, :album, :year, :comment, :track, :genre)
 
-    class ID3v1HeaderFormat < BinData::Record
+    class ID3v1TagFormat < BinData::Record
       string(:tag_id, :length => 3, :check_value => lambda { value == 'TAG' })
       string(:title, :length => 30)
       string(:artist, :length => 30)
@@ -12,6 +12,15 @@ module Mp3file
       string(:year, :length => 4)
       string(:comment, :length => 30)
       uint8(:genre_id)
+    end
+
+    def initialize(io)
+      tag = nil
+      begin
+        ID3v1TagFormat.read(io)
+      rescue BinData::ValidityError => ve
+        raise InvalidID3v1TagError, ve.message
+      end
     end
   end
 end
--- spec/mp3file/id3v1_tag_spec.rb
+++ spec/mp3file/id3v1_tag_spec.rb
@@ -3,9 +3,9 @@ require File.dirname(__FILE__) + '/../common_helpers'
 
 include CommonHelpers
 
-describe Mp3file::ID3v1Header do
+describe Mp3file::ID3v1Tag do
   it "rejects an ID3v1 tag if it doesn't begin with TAG" do
-    lambda { Mp3file::ID3v1Header.new(StringIO.new("\x00" * 128)) }.
-      should(raise_error(Mp3file::InvalidID3v1HeaderError))
+    lambda { Mp3file::ID3v1Tag.new(StringIO.new("\x00" * 128)) }.
+      should(raise_error(Mp3file::InvalidID3v1TagError))
   end
 end
