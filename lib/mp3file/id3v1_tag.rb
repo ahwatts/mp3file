fb36f4c9754582af285c9efa5b74ddbc4b6a2089
Andrew Watts <ahwatts@gmail.com>
2010-12-21 00:02:38 -0500

    Finish implementing ID3v1 tags

--- lib/mp3file/id3v1_tag.rb
+++ lib/mp3file/id3v1_tag.rb
@@ -14,13 +14,45 @@ module Mp3file
       uint8(:genre_id)
     end
 
+    # First group is the original spec, the second are Winamp extensions.
+    GENRES = 
+      %w{ Blues Classic\ Rock Country Dance Disco Funk Grunge Hip-Hop Jazz 
+        Metal New\ Age Oldies Other Pop R&B Rap Reggae Rock Techno Industrial 
+        Alternative Ska Death\ Metal Pranks Soundtrack Euro-Techno Ambient Trip-Hop 
+        Vocal Jazz+Funk Fusion Trance Classical Instrumental Acid House Game 
+        Sound\ Clip Gospel Noise AlternRock Bass Soul Punk Space Meditative 
+        Instrumental\ Pop Instrumental\ Rock Ethnic Gothic Darkwave Techno-Industrial 
+        Electronic Pop-Folk Eurodance Dream Southern\ Rock Comedy Cult Gangsta 
+        Top\ 40 Christian\ Rap Pop/Funk Jungle Native\ American Cabaret New\ Wave 
+        Psychadelic Rave Showtunes Trailer Lo-Fi Tribal Acid\ Punk Acid\ Jazz Polka 
+        Retro Musical Rock\ &\ Roll Hard\ Rock 
+
+        Folk Folk-Rock National\ Folk Swing 
+        Fast\ Fusion Bebob Latin Revival Celtic Bluegrass Avantgarde Gothic\ Rock 
+        Progressive\ Rock Psychedelic\ Rock Symphonic\ Rock Slow\ Rock Big\ Band 
+        Chorus Easy\ Listening Acoustic Humour Speech Chanson Opera Chamber\ Music 
+        Sonata Symphony Booty\ Bass Primus Porn\ Groove Satire Slow\ Jam Club Tango 
+        Samba Folklore Ballad Power\ Ballad Rhythmic\ Soul Freestyle Duet Punk\ Rock 
+        Drum\ Solo A\ capella Euro-House Dance\ Hall }
+
     def initialize(io)
-      tag = nil
+      @tag = nil
       begin
-        ID3v1TagFormat.read(io)
+        @tag = ID3v1TagFormat.read(io)
       rescue BinData::ValidityError => ve
         raise InvalidID3v1TagError, ve.message
       end
+
+      @title = @tag.title.split("\x00").first
+      @artist = @tag.artist.split("\x00").first
+      @album = @tag.album.split("\x00").first
+      @year = @tag.year
+      split_comment = @tag.comment.split("\x00").reject { |s| s == '' }
+      @comment = split_comment.first
+      if split_comment.size > 1
+        @track = split_comment.last.bytes.first
+      end
+      @genre = GENRES[@tag.genre_id]
     end
   end
 end
--- spec/mp3file/id3v1_tag_spec.rb
+++ spec/mp3file/id3v1_tag_spec.rb
@@ -8,4 +8,55 @@ describe Mp3file::ID3v1Tag do
     lambda { Mp3file::ID3v1Tag.new(StringIO.new("\x00" * 128)) }.
       should(raise_error(Mp3file::InvalidID3v1TagError))
   end
+
+  describe "When created with a properly-formatted ID3v1 tag" do
+    subject do
+      title = "Big Dipper"; title += "\x00" * (30 - title.size)
+      artist = "Cracker"; artist += "\x00" * (30 - artist.size)
+      album = "The Golden Age"; album += "\x00" * (30 - album.size)
+      year = "1996"
+      comment = "This is a comment"; comment += "\x00" * (30 - comment.size)
+      genre = 17.chr
+      Mp3file::ID3v1Tag.new(StringIO.new('TAG' + title + artist + album + year + comment + genre))
+    end
+
+    its(:title) { should == 'Big Dipper' }
+    its(:artist) { should == 'Cracker' }
+    its(:album) { should == 'The Golden Age' }
+    its(:year) { should == '1996' }
+    its(:comment) { should == 'This is a comment' }
+    its(:track) { should == nil }
+    its(:genre) { should == 'Rock' }
+  end
+
+  describe "When created with a properly-formatted ID3v1.1 tag" do
+    subject do
+      title = "Big Dipper"; title += "\x00" * (30 - title.size)
+      artist = "Cracker"; artist += "\x00" * (30 - artist.size)
+      album = "The Golden Age"; album += "\x00" * (30 - album.size)
+      year = "1996"
+      comment = "This is a comment"; comment += "\x00" * (29 - comment.size)
+      tracknum = 3.chr
+      genre = 17.chr
+      Mp3file::ID3v1Tag.new(StringIO.new('TAG' + title + artist + album + year + comment + tracknum + genre))
+    end
+
+    its(:title) { should == 'Big Dipper' }
+    its(:artist) { should == 'Cracker' }
+    its(:album) { should == 'The Golden Age' }
+    its(:year) { should == '1996' }
+    its(:comment) { should == 'This is a comment' }
+    its(:track) { should == 3 }
+    its(:genre) { should == 'Rock' }
+  end
+
+  describe "When created with a blank ID3v1 tag" do
+    subject { Mp3file::ID3v1Tag.new(StringIO.new("TAG" + ("\x00" * 125))) }
+    its(:title) { should == nil }
+    its(:artist) { should == nil }
+    its(:album) { should == nil }
+    its(:comment) { should == nil }
+    its(:track) { should == nil }
+    its(:genre) { should == 'Blues' }
+  end
 end
