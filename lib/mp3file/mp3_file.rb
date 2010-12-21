7444d4574f67205f79ea415c21b73f4f56bfe33f
Andrew Watts <ahwatts@gmail.com>
2010-12-21 00:51:30 -0500

    Get the MP3File code to read ID3v1 tags.

--- lib/mp3file/mp3_file.rb
+++ lib/mp3file/mp3_file.rb
@@ -7,6 +7,8 @@ module Mp3file
     attr_reader(:mpeg_version, :layer, :bitrate, :samplerate, :mode)
     attr_reader(:num_frames, :total_samples, :length)
 
+    attr_accessor(:id3v1_tag)
+
     def initialize(file_path)
       file_path = Pathname.new(file_path).expand_path if file_path.is_a?(String)
       load_file(file_path)
@@ -17,36 +19,65 @@ module Mp3file
     end
 
     def id3v1tag?
-      @has_id3v1tag
+      !@id3v1_tag.nil?
     end
 
     def id3v2tag?
-      @has_id3v2tag
+      !@id3v2_tag.nil?
+    end
+
+    def title
+      value_from_tags(:title)
+    end
+
+    def artist
+      value_from_tags(:artist)
+    end
+
+    def album
+      value_from_tags(:album)
+    end
+
+    def track
+      value_from_tags(:track)
+    end
+
+    def year
+      value_from_tags(:year)
+    end
+
+    def comment
+      value_from_tags(:comment)
+    end
+
+    def genre
+      value_from_tags(:genre)
     end
 
     private
+
+    def value_from_tags(v1_field)
+      if @id3v1_tag
+        @id3v1_tag.send(v1_field)
+      else
+        nil
+      end
+    end
 
     def load_file(file_path)
       @file = file_path.open('rb')
       @file.seek(0, IO::SEEK_END)
       @file_size = @file.tell
-      @file.seek(0, IO::SEEK_SET)
 
-      # Try to parse an ID3 header.
-      @id3v2tag = nil
-      idata = @file.read(10).unpack("a3CCB8N")
-      id3_indicator = idata[0]
-      id3_size = BitPaddedInt.unpad_number(idata[4])
-
-      if id3_indicator == "ID3" && id3_size > 0
-        # @id3v2tag = ID3v2::Tag.new(@file)
-        # @file.seek(@id3v2tag.size, IO::SEEK_SET)
-        @has_id3v2tag = true
-        @file.seek(id3_size, IO::SEEK_SET)
-      else
-        @has_id3v2tag = false
-        @file.seek(0, IO::SEEK_SET)
+      # Try to read an ID3v1 tag.
+      @id3v1_tag = nil
+      @file.seek(-128, IO::SEEK_END)
+      begin
+        @id3v1_tag = ID3v1Tag.new(@file)
+      rescue InvalidID3v1TagError => e
+        @id3v1_tag = nil
       end
+      @file.seek(0, IO::SEEK_SET)
 
       # Try to find the first MP3 header.
       @first_header_offset, @first_header = get_next_header(@file)
@@ -57,8 +88,8 @@ module Mp3file
       @samplerate = @first_header.samplerate
       @mode = @first_header.mode
       @audio_size = @file_size
-      if @id3v2tag
-        @audio_size -= @id3v2tag.size
+      if @id3v1_tag
+        @audio_size -= 128
       end
 
       # If it's VBR, there should be an Xing header after the
@@ -71,15 +102,6 @@ module Mp3file
         @file.seek(@first_header_offset + 4, IO::SEEK_CUR)
       end
 
-      # # There might be a VBRI header after 32 bytes
-      # if @xing_header.nil?
-      #   @file.seek(@first_header_offset + 36, IO::SEEK_SET)
-      #   xdata = @file.read(26).unpack("a4a*")
-      #   if xdata[0] == "VBRI"
-      #     raise NotImplementedError, "VBRI tags have not been implemented."
-      #   end
-      # end
-
       if @xing_header
         @vbr = true
         # Do the VBR length calculation.  What to do if we don't have
@@ -91,8 +113,7 @@ module Mp3file
           @bitrate = ((@xing_header.bytes.to_f / @length.to_f) * 8 / 1000).to_i
         end
       else
-        # Do the CBR length calculation.  Note that this will be
-        # off by about a second in the presence of an ID3v1 tag.
+        # Do the CBR length calculation.
         @vbr = false
         @num_frames = @audio_size / @first_header.frame_size
         @total_samples = @num_frames * @first_header.samples
--- spec/mp3file/mp3_file_spec.rb
+++ spec/mp3file/mp3_file_spec.rb
@@ -4,9 +4,10 @@ require File.dirname(__FILE__) + '/../common_helpers'
 include CommonHelpers
 
 describe Mp3file::MP3File do
-  describe "A 96 kbps 34 kHz Joint Stereo CBR file without any ID3 tags" do
+  describe "A 96 kbps 32 kHz Joint Stereo CBR file without any ID3 tags" do
     subject { Mp3file::MP3File.new(fixture_file('bret_96.mp3')) }
     its(:id3v2tag?) { should == false }
+    its(:id3v1tag?) { should == false }
     its("file.path") { should == fixture_file('bret_96.mp3').to_s }
     its("file.closed?") { should == true }
     its(:file_size) { should == fixture_file('bret_96.mp3').size }
@@ -26,6 +27,7 @@ describe Mp3file::MP3File do
   describe "a 44.1 kHz Stereo VBR file with an average bitrate of 13 kbps without any ID3 tags" do
     subject { Mp3file::MP3File.new(fixture_file('bret_vbr_6.mp3')) }
     its(:id3v2tag?) { should == false }
+    its(:id3v1tag?) { should == false }
     its("file.path") { should == fixture_file('bret_vbr_6.mp3').to_s }
     its("file.closed?") { should == true }
     its(:file_size) { should == fixture_file('bret_vbr_6.mp3').size }
@@ -40,6 +42,30 @@ describe Mp3file::MP3File do
     its(:total_samples) { should == 222336 }
     its(:length) { should == 5 }
     its(:vbr?) { should == true }
+  end
+
+  describe "A 96 kbps 32 kHz CBR file with only an ID3v1 tag" do
+    subject { Mp3file::MP3File.new(fixture_file('bret_id3v1.mp3')) }
+    its(:id3v2tag?) { should == false }
+    its(:id3v1tag?) { should == true }
+    its(:audio_size) { should == 60912 }
+    its(:first_header_offset) { should == 0 }
+    its(:mpeg_version) { should == 'MPEG 1' }
+    its(:layer) { should == 'Layer III' }
+    its(:bitrate) { should == 96 }
+    its(:samplerate) { should == 32000 }
+    its(:mode) { should == 'Joint Stereo' }
+    its(:num_frames) { should == 141 }
+    its(:total_samples) { should == 162432 }
+    its(:length) { should == 5 }
+    its(:vbr?) { should == false }
+    its('id3v1_tag.artist') { should == 'Cracker' }
+    its(:title) { should == 'Hey Bret (You Know What Time I' }
+    its(:album) { should == 'Sunrise in the Land of Milk an' }
+    its(:comment) { should == 'For testing the mp3file gem' }
+    its(:year) { should == '2009' }
+    its(:track) { should == 9 }
+    its(:genre) { should == 'Rock' }
   end
 
   # describe "A 96 kbps 34 kHz Joint Stereo CBR file with an ID3v2 tag" do
