3dcbd0b5c53c4463194abe0b884f9e2e3861c531
Andrew Watts <ahwatts@gmail.com>
2016-05-06 16:40:19 -0400

    Fix the Xing headers for the new bindata.

--- lib/mp3file/xing_header.rb
+++ lib/mp3file/xing_header.rb
@@ -5,21 +5,21 @@ module Mp3file
     attr_reader(:name, :frames, :bytes, :toc, :quality)
 
     class XingHeaderFormat < BinData::Record
-      string(:vbr_id, :length => 4, :check_value => lambda { value == 'Xing' || value == 'Info' })
+      string(:vbr_id, read_length: 4, assert: -> { value == 'Xing' || value == 'Info' })
 
-      uint8(:unused1, :check_value => lambda { value == 0 })
-      uint8(:unused2, :check_value => lambda { value == 0 })
-      uint8(:unused3, :check_value => lambda { value == 0 })
-      bit4(:unused4, :check_value => lambda { value == 0 })
+      uint8(:unused1, asserted_value: 0)
+      uint8(:unused2, asserted_value: 0)
+      uint8(:unused3, asserted_value: 0)
+      bit4(:unused4, asserted_value: 0)
       bit1(:quality_present)
       bit1(:toc_present)
       bit1(:bytes_present)
       bit1(:frames_present)
 
-      uint32be(:frames, :onlyif => lambda { frames_present == 1 })
-      uint32be(:bytes, :onlyif => lambda { bytes_present == 1 })
-      array(:toc, :type => :uint8, :read_until => lambda { index == 99 }, :onlyif => lambda { toc_present == 1 })
-      uint32be(:quality, :onlyif => lambda { quality_present == 1 })
+      uint32be(:frames, onlyif: -> { frames_present == 1 })
+      uint32be(:bytes, onlyif: -> { bytes_present == 1 })
+      array(:toc, type: :uint8, read_until: -> { index == 99 }, onlyif: -> { toc_present == 1 })
+      uint32be(:quality, onlyif: -> { quality_present == 1 })
     end
 
     def initialize(io)
